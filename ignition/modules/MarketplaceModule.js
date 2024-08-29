e5c094e09e4525d8b37d765aaaa59d02c38864a1
david-drvar <david.drvar.bogdanovic@gmail.com>
2024-08-29 23:22:11 +0200

    deployment - every contract deployed independently

--- ignition/modules/MarketplaceModule.js
+++ ignition/modules/MarketplaceModule.js
@@ -1,18 +1,9 @@
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-const UsersModule = require("./UsersModule");
-const EscrowModule = require("./EscrowModule");
 
 const MarketplaceModule = buildModule("MarketplaceModule", (m) => {
   const deployer = m.getAccount(0);
 
-  const { users } = m.useModule(UsersModule);
-  const { escrow } = m.useModule(EscrowModule);
-  // const users = m.contract("Users", [deployer]);
-  // const escrow = m.contract("Escrow", [deployer]);
-
-  const marketplace = m.contract("Marketplace", [deployer, escrow, users]);
-
-  m.call(escrow, "setMarketplaceContractAddress", [marketplace]);
+  const marketplace = m.contract("Marketplace", [deployer]);
 
   return { marketplace };
 });
