17861c2e960390a92bd22bed4d6f0a1c7732cef4
david-drvar <david.drvar.bogdanovic@gmail.com>
2024-09-20 15:06:07 +0200

    contract interfaces updated

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -5,10 +5,19 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 
 interface IMarketplace {
+    
     enum ItemStatus {
         LISTED,
         BOUGHT,
         DELETED
+    }
+
+    enum Condition {
+        NEW,
+        LIKE_NEW,
+        EXCELLENT,
+        GOOD,
+        DAMAGED
     }
 
     struct Item {
@@ -19,6 +28,11 @@ interface IMarketplace {
         string title;
         string[] photosIPFSHashes;
         ItemStatus itemStatus;
+        Condition condition;
+        string category;
+        string subcategory;
+        string country;
+        bool isGift;
     }
 
     event ItemListed(uint256 indexed id, address indexed seller, string title, string description, uint256 price, string[] photosIPFSHashes);
@@ -27,11 +41,40 @@ interface IMarketplace {
     event ItemDeleted(uint256 indexed id, address indexed seller);
 
     function setUsersContractAddress(address _usersContractAddress) external;
+
     function setEscrowContractAddress(address _escrowContractAddress) external;
-    function listNewItem(string memory _title, string memory _description, uint256 _price, string[] memory photosIPFSHashes) external;
-    function updateItem(uint256 id, string memory _title, string memory _description, uint256 _price, string[] memory photosIPFSHashes) external;
+
+    function listNewItem(
+        string memory _title, 
+        string memory _description, 
+        uint256 _price, 
+        string[] memory photosIPFSHashes, 
+        Condition _condition,
+        string memory _category,
+        string memory _subcategory,
+        string memory _country,
+        bool _isGift
+    ) external;
+
+    function updateItem(
+        uint256 id, 
+        string memory _title, 
+        string memory _description, 
+        uint256 _price, 
+        string[] memory photosIPFSHashes,
+        Condition _condition,
+        string memory _category,
+        string memory _subcategory,
+        string memory _country,
+        bool _isGift
+    ) external;
+
     function deleteItem(uint256 id) external;
+
     function buyItem(address sellerAddress, uint256 id, address _moderator) external payable;
+
+    function buyItemWithoutModerator(address sellerAddress, uint256 id) external payable;
+
     function getItemCount() external view returns (uint256);
 }
 
--- contracts/IUsers.sol
+++ contracts/IUsers.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.24;
 
 interface IUsers {
+
     struct UserProfile {
         address userAddress;
         string username;
@@ -16,23 +17,23 @@ interface IUsers {
         uint8 moderatorFee;
     }
 
-    event UserRegistered(address indexed userAddress, string indexed username, string firstName,
+    event UserRegistered(address indexed userAddress, string username, string firstName,
         string lastName, string country, string description, string email, string avatarHash, bool isModerator, uint8 moderatorFee);
-    event UserUpdated(address indexed userAddress, string indexed username, string firstName,
+    event UserUpdated(address indexed userAddress, string username, string firstName,
         string lastName, string country, string description, string email, string avatarHash, bool isModerator, uint8 moderatorFee);
-    event UserDeleted(address indexed userAddress, string indexed username);
+    event UserDeleted(address indexed userAddress, string username);
 
     function createProfile(string memory _username, string memory _firstName, string memory _lastName, string memory _country,
         string memory _description, string memory _email, string memory _avatarHash, bool _isModerator, uint8 _moderatorFee) external;
-    
+
     function updateProfile(string memory _username, string memory _firstName, string memory _lastName, string memory _country,
         string memory _description, string memory _email, string memory _avatarHash, bool _isModerator, uint8 _moderatorFee) external;
-    
+
     function deleteProfile() external;
-    
+
     function isRegisteredUser(address _user) external view returns (bool);
-    
+
     function isModerator(address _user) external view returns (bool);
-    
+
     function getProfile(address _user) external view returns (UserProfile memory);
 }
